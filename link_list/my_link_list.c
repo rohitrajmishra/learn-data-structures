c1403a9626b1a85f353edcbbe7a83371d4abf887
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2018-09-29 15:33:22 +0530

    Added print function to link list

--- link_list/my_link_list.c
+++ link_list/my_link_list.c
@@ -5,8 +5,21 @@ struct Node {
 	struct Node *next;
 };
 
+Node *head;
+
+int Print() {
+	Node *temp = head;	
+	while(temp->next != NULL)
+	{
+		printf("%d ", temp->data);			
+		temp = temp->next;
+	}
+	printf("\n");
+	return 0;
+}
 int main(void) {
 	//Link list implementation code
+	Print();
 	return 0;
 }
 
